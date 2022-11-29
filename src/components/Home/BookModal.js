00809651b0a9d66e60e278399212850d1c3fdd5b
emon-webdev <emon.webdev@gmail.com>
2022-11-29 20:01:54 +0600

    modal change

--- src/components/Home/BookModal.js
+++ src/components/Home/BookModal.js
@@ -10,122 +10,121 @@ const BookModal = ({ booking, bookingData, closeModal }) => {
       <div className="modal">
         <div className="modal-box">
           <form onSubmit={booking}>
-            {/* {/ name field  /} */}
             <div className=" flex  font-bold text-lg">
               <label className="label">
                 <span className="label-text">Product ID: </span>
               </label>
               <input disabled name="productId" defaultValue={_id} type="text" />
             </div>
-            {/* {/ seller name  /} */}
-            <div className=" flex  font-bold text-lg">
+            <div className="  font-bold text-lg">
               <label className="label">
                 <span className="label-text">Seller Name: </span>
               </label>
               <input
                 disabled
-                name="sellerName"
+                className="input input-bordered w-full"
+                name="sellerName "
                 defaultValue={sellerName}
                 type="text"
               />
             </div>
-            {/* {/ seller email  /} */}
-            <div className=" flex  font-bold text-lg">
+            <div className="  font-bold text-lg">
               <label className="label">
                 <span className="label-text">Seller Email: </span>
               </label>
               <input
                 disabled
                 name="sellerEmail"
+                className="input input-bordered w-full"
                 defaultValue={email}
                 type="text"
               />
             </div>
-            {/* {/ seller phone number  /} */}
-            <div className=" flex  font-bold text-lg">
+            <div className=" font-bold text-lg">
               <label className="label">
                 <span className="label-text">Seller phone number: </span>
               </label>
               <input
                 disabled
                 name="sellerNumber"
+                className="input input-bordered w-full"
                 defaultValue={number}
                 type="text"
               />
             </div>
-            {/*buyer name name field  */}
-            <div className=" flex  font-bold text-lg">
+            <div className="   font-bold text-lg">
               <label className="label">
                 <span className="label-text">Your Name: </span>
               </label>
               <input
                 disabled
                 name="buyerName"
+                className="input input-bordered w-full"
                 defaultValue={user.displayName}
                 type="text"
               />
             </div>
-            {/* {/ email field  /} */}
-            <div className=" flex  mt-4 font-bold text-lg">
+            <div className="  mt-4 font-bold text-lg">
               <label className="label">
                 <span className="label-text">Your Email: </span>
               </label>
               <input
                 disabled
                 name="buyerEmail"
+                className="input input-bordered w-full"
                 defaultValue={user.email}
                 type="text"
               />
             </div>
-            <div className=" flex  mt-4 font-bold text-lg">
+            <div className="  mt-4 font-bold text-lg">
               <label className="label">
                 <span className="label-text">Product Name: </span>
               </label>
               <input
                 disabled
+                className="input input-bordered w-full"
                 name="title"
                 defaultValue={title}
                 type="text"
               />
             </div>
-            <div className=" flex  mt-4 font-bold text-lg">
+            <div className="  mt-4 font-bold text-lg">
               <label className="label">
                 <span className="label-text">price: </span>
               </label>
               <input
                 disabled
-                name="resalePrice"
+                name="resalePrice "
                 defaultValue={resalePrice}
+                className="input input-bordered w-full"
                 type="text"
               />
             </div>
-            <div className=" flex  mt-4 font-bold text-lg">
+
+            <div className="  mt-4 font-bold text-lg">
               <label className="label">
                 <span className="label-text">Your Phone Number: </span>
               </label>
               <input
                 required
                 name="buyerMobile"
-                className="input input-bordered"
+                className="input input-bordered w-full"
                 type="phone"
               />
             </div>
-            <div className=" flex  mt-4 font-bold text-lg">
+            <div className="  mt-4 font-bold text-lg">
               <label className="label">
                 <span className="label-text">Meeting Location: </span>
               </label>
               <input
                 required
                 name="meetingLocation"
-                className="input input-bordered"
+                className="input input-bordered w-full"
                 type="text"
               />
             </div>
-            <div className="modal-action">
-              <button
-                type="submit"
-                className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white "
-              >
+            <div className="modal-action flex justify-evenly">
+              <button type="submit" className="btn btn-primary  ">
                 Submit
               </button>
               <label onClick={closeModal} className="btn">
